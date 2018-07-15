6a4214bf952e8eb729459e390dd1ead93aa931c8
Kyle <kyle.ratti@outlook.com>
2018-07-15 11:53:14 -0400

    Added initial video get request handling

--- src/controllers/public.controller.ts
+++ src/controllers/public.controller.ts
@@ -1,4 +1,7 @@
 import { Router, Request, Response } from 'express';
+import HttpStatus from 'http-status-codes';
+
+import { Video } from "../models/video";
 
 const router: Router = Router();
 
@@ -9,7 +12,25 @@ router.get('/', (req: Request, res: Response) => {
 
 router.get('/:redditPostId', (req: Request, res: Response) => {
     let redditPostId = req.params.redditPostId;
-    res.send('Hello reddit post ' + redditPostId);
+
+    Video.find({
+        where: {
+            redditPostId: redditPostId
+        },
+        limit: 1
+    }).then(vid => {
+        if(vid) {
+            res.status(HttpStatus.OK).render('show', {
+                redditPostId: redditPostId,
+                posterLocation: 'http://google.com',
+                videoLocation: 'http://google.com'
+            });
+        }
+
+        return res.status(HttpStatus.NOT_FOUND).render('errors/404', {
+            message: 'This video was not found in the database. Typically this means a-mirror has not been asked to mirror this post or doesn\'t have an agreement in place with the subreddit moderators to mirror links.'
+        });
+    });
 });
 
 export const PublicController: Router = router;
