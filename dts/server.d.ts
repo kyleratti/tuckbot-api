15e8dc04bab2a178b0f51b3ffedabed049c915bc
Kyle <kyle.ratti@outlook.com>
2018-07-15 11:33:40 -0400

    Removed all dts files

--- dts/server.d.ts
+++
@@ -1,8 +0,0 @@
-import { Database } from './db/database';
-export declare var database: Database;
-export default class Server {
-    private app;
-    private port;
-    constructor();
-    start(): void;
-}
