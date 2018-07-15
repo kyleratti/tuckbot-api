02c508c2669896a899423ac872a11aaeaa5a35d4
Kyle <kyle.ratti@outlook.com>
2018-07-15 11:33:11 -0400

    Removed DTS files

--- dts/db/database.d.ts
+++
@@ -1,7 +0,0 @@
-import Sequelize from 'sequelize';
-export declare class Database {
-    private dbLocation;
-    db: Sequelize.Sequelize;
-    constructor(dbLocation: String);
-    connect(): void;
-}
