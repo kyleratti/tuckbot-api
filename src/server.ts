a17d812aaf76271373ce91fc04a5b3146f4fc406
Kyle <kyle.ratti@outlook.com>
2018-07-16 19:03:04 -0400

    Fixed casing, reordered classes

--- src/server.ts
+++ src/server.ts
@@ -16,9 +16,38 @@ const config = configurator.load();
 // load database
 export var database = new Database(config.database.location);
 
+export class WebServer {
+    private app: express.Application;
+    private port: number;
+    
+    constructor() {
+        let app = express();
+        let port = config.app.webPort || 3000;
+
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());
+        app.use(fileUpload());
+
+        app.set('view engine', 'pug');
+
+        app.use('/', PublicController);
+
+        this.app = app;
+        this.port = port;
+    }
+
+    start() {
+        database.connect();
+
+        this.app.listen(this.port, () => {
+            console.log(`listening for web requests at http://127.0.0.1:${this.port}`);
+        })
+    }
+}
+
 export class CdnServer {
     private app: express.Application;
-    private port: Number;
+    private port: number;
 
     constructor() {
         let app = express();
@@ -34,38 +63,33 @@ export class CdnServer {
         this.app.use('/video', express.static(config.app.file.local.storageDir));
 
         this.app.listen(this.port, () => {
-            console.log(`Listening for CDN requests at http://127.0.0.1:${this.port}`);
+            console.log(`listening for cdn requests at http://127.0.0.1:${this.port}`);
         });
     }
 }
 
-export class WebServer {
+export class ApiServer {
     private app: express.Application;
-    private port: Number;
-    
+    private port: number;
+ 
     constructor() {
         let app = express();
-        let port = config.app.webPort || 3000;
+        let port = config.app.apiPort || 3002;
 
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(bodyParser.json());
         app.use(fileUpload());
 
-        app.set('view engine', 'pug');
-
-        app.use('/', PublicController);
-        app.use('/api', APIController);
+        app.use('/', APIController);
 
         this.app = app;
         this.port = port;
     }
 
     start() {
-        database.connect();
-
         this.app.listen(this.port, () => {
-            console.log(`Listening for web requests at http://127.0.0.1:${this.port}`);
-        })
+            console.log(`listening for api requests at http://127.0.0.1:${this.port}`);
+        });
     }
 }
 
