c44f4183adb81e892fb01e612ae43fdc6e26a216
Kyle <kyleratti@users.noreply.github.com>
2018-07-22 17:24:05 -0400

    Added Mobility Mary support

--- src/controllers/public.controller.ts
+++ src/controllers/public.controller.ts
@@ -14,6 +14,31 @@ router.get('/', (req: Request, res: Response) => {
     res.send('Hello, world!');
 });
 
+router.get('/mobilitymary', (req, res) => {
+    return res.status(HttpStatus.OK).render('mobilitymary', {
+        title: "Mirrors of Mobility Mary's finest work",
+        stylesheet: makeUrl(UrlType.Cdn, '/css/style.css'),
+        serverName: os.hostname().split('.')[0],
+
+        posts: [
+            { redditPostId: "90p8rr", title: "Mobility Mary viciously mauled by rabid beast" },
+            { redditPostId: "90nw6g", title: "Not to be out done by Mobility Mary, our buddy—who I’m dubbing “Court Order Chris”— confronts a police chief." },
+            { redditPostId: "90w7xd", title: "Don’t tell Mobility Mary what to do" },
+            { redditPostId: "90tdlu", title: "Mobility Mary Catching a Pokémon" },
+            { redditPostId: "90u9te", title: "Mobility Mary attacked; hit in head four times" },
+            { redditPostId: "90w36k", title: "Mobility Mary Makes a Friend" },
+            { redditPostId: "90umd0", title: "MOBILITY MARY MAKES PICKLE RELISH" },
+            { redditPostId: "90w6g1", title: "Mobility Mary Barely Escapes Uninjured From Intentional Road Rage Attack" },
+            { redditPostId: "90u8vm", title: "The various obstacles Mobility Mary faces on the sidewalk, all in the span of 30 seconds" },
+            { redditPostId: "90sb7z", title: "Mobility Mary in the trenches" },
+            { redditPostId: "90k61m", title: "Mobility Mary in a construction zone" },
+            { redditPostId: "90w64l", title: "Mobility Mary Barely Escapes an Escape" },
+            { redditPostId: "90oejj", title: "Mobility Mary vs Unleashed Dog" },
+            { redditPostId: "90ohib", title: "NEW Mobility Mary from today - this time she is attacked by unleashed dogs and barely escapes with her life" }
+        ]
+    })
+});
+
 router.get('/:redditPostId', (req: Request, res: Response) => {
     let redditPostId = req.params.redditPostId;
 
