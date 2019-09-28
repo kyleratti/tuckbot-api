796bfa318e5814564290b946a8bbefb722a36519
Kyle <kyleratti@users.noreply.github.com>
2019-09-27 22:25:32 -0400

    Removed old web servers

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import { WebServer, CdnServer, ApiServer } from './server';
+import { ApiServer } from "./server";
 
 function run() {
-    let webServer = new WebServer();
-    webServer.start();
-
-    let cdnServer = new CdnServer();
-    cdnServer.start();
-
-    let apiServer = new ApiServer();
-    apiServer.start();
+  let apiServer = new ApiServer();
+  apiServer.start();
 }
 
 run();
