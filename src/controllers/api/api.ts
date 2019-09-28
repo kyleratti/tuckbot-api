42a7e6926c5c0098fef06279785b514232f87cf3
Kyle <kyleratti@users.noreply.github.com>
2019-09-27 22:17:16 -0400

    Added generic API functions

--- src/controllers/api/api.ts
+++ src/controllers/api/api.ts
@@ -1,6 +1,18 @@
 import { Response } from "express";
 import { ResponseData } from "../../structures";
 
+let appToken = "TODO"; // FIXME: read app token from configurator
+
+/**
+ * Checks if the specified request is authorized
+ * @param req The request to evaluate
+ */
+function authorized(req): boolean {
+  if (req.method === "GET") return req.headers.token === appToken;
+
+  return req.body && req.body.token === appToken;
+}
+
 export function response(
   res: Response,
   data: ResponseData
