114f189b7d8876c740e6b7464e910f936b966f28
Kyle Ratti <kyleratti@users.noreply.github.com>
2020-01-30 21:46:53 -0500

    Fixed filename

--- src/migration/1580438784594-ResetPrunedVideosToNull.ts
+++ src/migration/1580438784594-ResetPrunedVideosToNull.ts
@@ -1,6 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class ResetPrunedVideosToNull1580437738 implements MigrationInterface {
+export class ResetPrunedVideosToNull1580438784594
+  implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(`UPDATE video SET lastPrunedAt = null`);
   }
