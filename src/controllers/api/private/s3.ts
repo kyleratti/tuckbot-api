6fc23723f1fcfe93994f21530d043ec55a7ef0bc
Kyle <kyleratti@users.noreply.github.com>
2020-05-26 22:40:54 -0400

    Update s3.ts

--- src/controllers/api/private/s3.ts
+++ src/controllers/api/private/s3.ts
@@ -48,33 +48,52 @@ router.get("/all", async (_req, res) => {
 router.delete("/prune", async (req, res) => {
   const objects = await S3Endpoint.getAll();
   const files = objects.map((obj) => obj.key);
-  const mirrors = await Video.find({
+  const mirrorsQuery = await Video.find({
+    select: ["redditPostId"],
     order: {
       createdAt: "ASC",
     },
   });
-  const purged = new Array<string>();
+  const mirrors = mirrorsQuery.map((mirror) => mirror.redditPostId);
+
+  const [purgedS3, purgedApi] = [new Array<string>(), new Array<string>()];
+
+  objects.forEach((obj) => {
+    const [key, redditPostId] = [obj.key, obj.key.split(".")[0]];
+    const exists = mirrors.find((mirror) => mirror === redditPostId) !== null;
+
+    if (!exists) {
+      // S3Endpoint.delete(key);
+      req.log.info(
+        `Reddit Post ID '%s' exists in object storage but missing from tuckbot; removing from object storage`,
+        redditPostId
+      );
+
+      purgedS3.push(key);
+    }
+  });
 
   mirrors.forEach((mirror) => {
-    const key = `${mirror.redditPostId}.mp4`;
+    const key = `${mirror}.mp4`;
     const exists = files.includes(key);
 
     if (!exists) {
       // mirror.remove();
       req.log.info(
         `Reddit Post ID '%s' exists but '%s' missing from object storage; removing`,
-        mirror.redditPostId,
+        mirror,
         key
       );
 
-      purged.push(key);
+      purgedApi.push(key);
     }
   });
 
   return response(res, {
     data: {
-      count: purged.length,
-      purged: purged,
+      count: purgedS3.length + purgedApi.length,
+      purgedS3: purgedS3.length,
+      purgedApi: purgedApi.length,
     },
   });
 });
