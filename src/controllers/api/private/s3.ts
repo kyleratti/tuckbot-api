bd865e8bd1da9e09bb9aecf3bacf28ae24beb5b4
Kyle <kyleratti@users.noreply.github.com>
2020-05-26 20:26:34 -0400

    Add purge endpoint

--- src/controllers/api/private/s3.ts
+++ src/controllers/api/private/s3.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import HttpStatusCode from "http-status-codes";
 import { configurator } from "tuckbot-util";
 import { response } from "../";
+import { Video } from "../../../entity";
 import { S3Endpoint } from "../../../services";
 
 const router = Router();
@@ -40,6 +41,40 @@ router.get("/all", async (_req, res) => {
     data: {
       count: files.length,
       files: files,
+    },
+  });
+});
+
+router.delete("/prune", async (req, res) => {
+  const objects = await S3Endpoint.getAll();
+  const files = objects.map((obj) => obj.key);
+  const mirrors = await Video.find({
+    order: {
+      createdAt: "ASC",
+    },
+  });
+  const purged = new Array<string>();
+
+  mirrors.forEach((mirror) => {
+    const key = `${mirror.redditPostId}.mp4`;
+    const exists = files.includes(key);
+
+    if (!exists) {
+      // mirror.remove();
+      req.log.info(
+        `Reddit Post ID '%s' exists but '%s' missing from object storage; removing`,
+        mirror.redditPostId,
+        key
+      );
+
+      purged.push(key);
+    }
+  });
+
+  return response(res, {
+    data: {
+      count: purged.length,
+      purged: purged,
     },
   });
 });
