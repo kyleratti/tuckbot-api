79fcdc5276e2b40dc4016e1d67212ecc788c7681
Kyle <kyleratti@users.noreply.github.com>
2020-06-17 11:05:22 -0400

    Update Video subscriber to use tuckbot-util/logger

--- src/subscriber/video.ts
+++ src/subscriber/video.ts
@@ -1,10 +1,10 @@
+import { logger } from "tuckbot-util";
 import {
   EntitySubscriberInterface,
   EventSubscriber,
   InsertEvent,
 } from "typeorm";
 import { Video } from "../entity";
-import { logger } from "../server";
 import { ACMApi } from "../services";
 
 @EventSubscriber()
@@ -14,13 +14,30 @@ export class VideoSubscriber implements EntitySubscriberInterface<Video> {
   }
 
   async afterInsert(event: InsertEvent<Video>) {
+    const video = event.entity;
+    const videoDetails = {
+      id: video.id,
+      redditPostId: video.redditPostId,
+      redditPostTitle: video.redditPostTitle,
+      mirrorUrl: video.mirrorUrl,
+    };
+
     try {
       await ACMApi.update({
         redditPostId: event.entity.redditPostId,
         url: event.entity.mirrorUrl,
       });
-    } catch (e) {
-      logger.fatal(e);
+
+      logger.info({
+        msg: `Updated video link with a-centralized-mirror API`,
+        video: videoDetails,
+      });
+    } catch (err) {
+      logger.fatal({
+        msg: `Unable to update video link with a-centralized-mirror API`,
+        video: videoDetails,
+        error: err,
+      });
     }
   }
 }
